efed9adc4ba9f1f9318a73c30f3a194a02c5258b
AdriiiRusso <adriii.ru@gmail.com>
2017-04-03 11:05:00 -0300

    Add files via upload

---
+++ Adición.cpp
@@ -0,0 +1,24 @@
+/*
+	"Adición"
+	"Adrián Russo"
+	"20170403"
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+	double A;
+	double B;
+	
+	cout << "Suma de A + B:" << endl;
+	cout << "Ingrese A: ";
+	cin >> A;
+	cout << "Ingrese B: ";
+	cin >> B;
+	cout << "La suma de A y B es: " << A + B << endl;
+	
+	system ("PAUSE");
+}
